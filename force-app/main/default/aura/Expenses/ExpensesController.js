2660f73cbe6f54ed53c291f80ed4ceed52671884
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-04-17 17:29:27 +1000

    Update ExpensesController

--- force-app/main/default/aura/Expenses/ExpensesController.js
+++ force-app/main/default/aura/Expenses/ExpensesController.js
@@ -1,5 +1,23 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
+  doInit: function(component, event, helper) {
+    // Create Action from Apex Controller
+    var action = component.get('c.getExpenses')
+    console.log('before action.setCallback')
+    // Callback to Action
+    action.setCallback(this, function(response) {
+      console.log('inside action.setCallback')
+      var state = response.getState()
+      console.log('after response.getState')
+      if (state === 'SUCCESS') {
+        console.log('SUCCESS')
+        // Display list to view
+        component.set('v.expenses', response.getReturnValue())
+        console.log('response.getReturnValue()', response.getReturnValue())
+      } else {
+        // Log error
+        console.log('Failed with state: ' + state)
+      }
+    })
+    $A.enqueueAction(action)
+  }
 })
